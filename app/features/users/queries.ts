a44ca680fb413c8c9de5ebc2d1a4b34015650fcd
bellaria19 <current11@naver.com>
2025-09-20 03:00:08 +0900

    Add check user logged in

--- app/features/users/queries.ts
+++ app/features/users/queries.ts
@@ -1,6 +1,8 @@
 import type { SupabaseClient } from '@supabase/supabase-js';
 import type { Database } from 'database.types';
 
+import { redirect } from 'react-router';
+
 export const getUser = async (
   client: SupabaseClient<Database>,
   { username }: { username: string }
@@ -20,6 +22,7 @@ export const getUserById = async (
   client: SupabaseClient<Database>,
   { id }: { id: string }
 ) => {
+  console.log('id getUserById', id);
   const { data, error } = await client
     .from('profiles')
     .select('*')
@@ -28,9 +31,18 @@ export const getUserById = async (
   if (error) {
     throw error;
   }
-  console.log('data');
-  console.log(data);
-  console.log('error');
-  console.log(error);
+  console.log('data getUserById', data);
+  console.log('error getUserById', error);
+
   return data;
+};
+
+export const getLoggedInUserId = async (client: SupabaseClient<Database>) => {
+  const { data, error } = await client.auth.getUser();
+
+  if (error || !data.user) {
+    throw redirect('/auth/login');
+  }
+
+  return data.user.id;
 };
